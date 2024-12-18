1ec18c09b94925217d87d7cc4d66f898bd0bce3c
alaahemdan1 <alaahemdan2021@gmail.com>
2024-12-18 14:03:47 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './nav/nav.component'; // التأكد من استيراد NavComponent بشكل صحيح
 import { GiftsComponent } from './gifts/gifts.component';
 import { FooterComponent } from './footer/footer.component';
 import { PlamComponent } from './plam/plam.component';
@@ -14,7 +13,7 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
+    NavComponent,  // تم التأكد من إضافة NavComponent هنا
     GiftsComponent,
     FooterComponent,
     PlamComponent,
@@ -27,7 +26,7 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration()  // يمكن الإبقاء على هذه الخدمة إذا كنت بحاجة إليها
   ],
   bootstrap: [AppComponent]
 })
